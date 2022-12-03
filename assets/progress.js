0123352a9ac3c4631e59c53e2741ef70946934c1
Zach Cohn <85762393+zachicecreamcohn@users.noreply.github.com>
2022-12-03 09:16:19 -0600

    asdf

---
+++ assets/progress.js
@@ -0,0 +1,22 @@
+// make a fetch request to the url
+let URL = "https://zachcohn.me/letseat/api/charge_sucess"
+
+fetch(URL, {
+    method: "GET",
+    headers: {
+        "Content-Type": "application/json"
+        }
+        })
+        .then(response => response.json())
+        .then(data => {
+            let total = data.total;
+            // make sure total has two decimal places
+            total = total.toFixed(2);
+            // set the total to the total element
+            document.getElementById("progress-total").innerHTML = `$${total}`;
+        })
+        .catch(error => {
+            console.error(error);
+        });
+
+        
